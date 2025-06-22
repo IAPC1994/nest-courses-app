79e417aacc78ac9e46d98c35633cb39e9f94d82b
Ivan Panussis C <ivan.panussis.c@gmail.com>
2025-06-22 20:23:53 +1000

    fix: Just admin can create, update or delete courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -13,12 +13,15 @@ import { CoursesService } from './courses.service';
 import { CreateCourseDto } from './dto/create-course.dto';
 import { UpdateCourseDto } from './dto/update-course.dto';
 import { PaginationDto } from 'src/common/dtos/pagination.dto';
+import { ValidRoles } from 'src/auth/interfaces';
+import { Auth } from 'src/auth/decorators';
 
 @Controller('courses')
 export class CoursesController {
   constructor(private readonly coursesService: CoursesService) {}
 
   @Post()
+  @Auth(ValidRoles.admin)
   create(@Body() createCourseDto: CreateCourseDto) {
     return this.coursesService.create(createCourseDto);
   }
@@ -34,6 +37,7 @@ export class CoursesController {
   }
 
   @Patch(':id')
+  @Auth(ValidRoles.admin)
   update(
     @Param('id', ParseUUIDPipe) id: string,
     @Body() updateCourseDto: UpdateCourseDto,
@@ -42,6 +46,7 @@ export class CoursesController {
   }
 
   @Delete(':id')
+  @Auth(ValidRoles.admin)
   remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.coursesService.remove(id);
   }
--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -4,10 +4,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { CoursesService } from './courses.service';
 import { CoursesController } from './courses.controller';
 import { Course } from './entities/course.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [CoursesController],
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course]), AuthModule],
   providers: [CoursesService],
   exports: [TypeOrmModule, CoursesService],
 })
